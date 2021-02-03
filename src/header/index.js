6dc7225d53d5d1ece77eb31d24e8e729849ba582
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-02-03 14:37:19 +0200

    Feat: header component

---
+++ src/header/index.js
@@ -0,0 +1,11 @@
+import './index.module.css';
+
+function Header() {
+    return (
+        <div className="list-books-title">
+            <h1>MyReads</h1>
+        </div>
+    );
+}
+
+export default Header;
