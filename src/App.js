278591d408b1959c0b7ff9da4de54278b8b5261e
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-02-03 15:02:31 +0200

    Feat: add book button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from './header';
-
+import AddBook from './main/add-book';
 function App() {
   return (
     <div className="App">
       Hello World!
+      <AddBook />
     </div>
   );
 }
